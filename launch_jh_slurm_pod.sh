269d63fe408cc4a0b68f593314fd6b35b5dbfcd2
James C. Womack <J.C.Womack@bristol.ac.uk>
2024-10-23 16:26:17 +0100

    Script to bring up podman pod from manifest
    * Rename named volume
    * Comment with information on working with volume and pod
    * Tested on macOS with podman machine

--- launch_jh_slurm_pod.sh
+++ launch_jh_slurm_pod.sh
@@ -7,21 +7,30 @@ set -x
 podman build -t brics_jupyterhub ./brics_jupyterhub
 podman build -t brics_slurm ./brics_slurm
 
-# Create podman named volume
-podman volume create jupyterhub_root_vol
+# Create podman named volume containing JupyterHub data
+podman volume create jupyterhub_root
 if [[ $(uname) == "Darwin" ]]; then
   # podman volume import not available using remote client, so run podman inside VM
-  tar --cd brics_jupyterhub_root/ --create --file - . | podman machine ssh podman volume import jupyterhub_root_vol -
+  tar --cd brics_jupyterhub_root/ --create --file - . | podman machine ssh podman volume import jupyterhub_root -
 else
-  tar --cd brics_jupyterhub_root/ --create --file - . | podman volume import jupyterhub_root_vol -
+  tar --cd brics_jupyterhub_root/ --create --file - . | podman volume import jupyterhub_root -
 fi
 
-podman kube play --replace jh_slurm_pod.yaml
+podman kube play jh_slurm_pod.yaml
+
+# To inspect contents of volume (extract into current directory)
+# macOS
+#   podman machine ssh podman volume export jupyterhub_root | tar --extract --verbose
+# Linux
+#   podman volume export jupyterhub_root | tar --extract --verbose
 
 # To tear down pod
-# podman kube down jh_slurm_pod.yaml 
+#   podman kube down jh_slurm_pod.yaml 
+# or
+#   podman pod stop jupyterhub-slurm
+#   podman pod rm jupyterhub-slurm
 
-# To remove volme
-# podman volume rm jupyterhub_root_vol
+# To remove volume
+#   podman volume rm jupyterhub_root_vol
 
 set +x
