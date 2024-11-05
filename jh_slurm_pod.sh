b1d0d6600feaab7a05197be882dd154fc8293db6
James C. Womack <J.C.Womack@bristol.ac.uk>
2024-11-05 11:42:26 +0000

    Explicitly set user (UID) and group (GID) when creating named volumes
    Also exclude .gitkeep from volume

--- jh_slurm_pod.sh
+++ jh_slurm_pod.sh
@@ -3,6 +3,18 @@ set -euo pipefail
 
 USAGE='./jh_slurm_pod.sh {up|down}'
 
+# Get user and group for JupyterHub container volume from environment, or set defaults
+: ${JUPYTERUSER:=root}
+: ${JUPYTERUSER_UID:=0}
+: ${JUPYTERGROUP:=root}
+: ${JUPYTERGROUP_UID:=0}
+
+# Get user and group for Slurm container volume from environment, or set defaults
+: ${SLURMUSER:=slurm}
+: ${SLURMUSER_UID:=64030}
+: ${SLURMGROUP=slurm}
+: ${SLURMGROUP_GID:=64030}
+
 # Creates an SSH key in the directory provided as an argument, then writes a 
 # K8s manifest for a Secret containing the key data to stdout
 function make_ssh_key_secret {
@@ -39,8 +51,13 @@ function bring_pod_up {
   podman volume create jupyterhub_root
   if [[ $(uname) == "Darwin" ]]; then
     # podman volume import not available using remote client, so run podman inside VM
-    tar --cd brics_jupyterhub_root/ --create --file - . | podman machine ssh podman volume import jupyterhub_root -
+    tar --cd brics_jupyterhub_root/ --create \
+      --exclude .gitkeep \
+      --uname ${JUPYTERUSER} --uid ${JUPYTERUSER_UID} \
+      --gname ${JUPYTERGROUP} --gid ${JUPYTERGROUP_GID} \
+      --file - . | podman machine ssh podman volume import jupyterhub_root -
   else
+    # TODO update GNU tar command to exclude .gitkeep and set UID/GID for files in archive
     tar --cd brics_jupyterhub_root/ --create --file - . | podman volume import jupyterhub_root -
   fi
 
@@ -48,7 +65,11 @@ function bring_pod_up {
   podman volume create slurm_root
   if [[ $(uname) == "Darwin" ]]; then
     # podman volume import not available using remote client, so run podman inside VM
-    tar --cd brics_slurm_root/ --create --file - . | podman machine ssh podman volume import slurm_root -
+    tar --cd brics_slurm_root/ --create \
+      --exclude .gitkeep \
+      --uname ${SLURMUSER} --uid ${SLURMUSER_UID} \
+      --gname ${SLURMGROUP} --gid ${SLURMGROUP_GID} \
+      --file - . | podman machine ssh podman volume import slurm_root -
   else
     tar --cd brics_slurm_root/ --create --file - . | podman volume import slurm_root -
   fi
